1078d1e6c9ef97f082b7ae5ae62cef71258ca00f
BaoLong <phanhabaolong@gmail.com>
2024-09-24 15:30:46 +0700

    Update main

--- STM32/Core/Src/software_timer.c
+++ STM32/Core/Src/software_timer.c
@@ -12,10 +12,14 @@ void setTimer1(int duration) {
 	timer1_flag = 0;
 }
 void timerRun() {
-	if (timer1_counter <= 0) {
-		timer1_flag = 1;
+	if (timer1_counter > 0) {
+		timer1_counter--;
 
+		if (timer1_counter <= 0) {
+			timer1_flag = 1;
+
+		}
 	}
-	timer1_counter--;
+
 }
 
