2a004a94fae469b7ec52cbaf7ad356d45b501a3f
BaoLong <phanhabaolong@gmail.com>
2024-09-27 20:23:09 +0700

    Update Ex9

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -91,7 +91,33 @@ void updateLEDMatrix(int index) {
 		break;
 	}
 }
+void update7SEG(int index) {
+	turnoffLEDs();
+	switch (index) {
 
+	case 0:
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+		display7SEG(led_buffer[0]);
+		break;
+	case 1:
+
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+		display7SEG(led_buffer[1]);
+		break;
+	case 2:
+
+		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+		display7SEG(led_buffer[2]);
+		break;
+	case 3:
+
+		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
+		display7SEG(led_buffer[3]);
+		break;
+	default:
+		break;
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -148,7 +174,7 @@ int main(void) {
 				hour = 0;
 			}
 			updateClockBuffer();
-			//update7SEG(index_led);
+			update7SEG(index_led);
 			index_led++;
 			if (index_led >= 4) {
 				index_led = 0;
