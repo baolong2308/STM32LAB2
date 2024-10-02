8e131b443cb5090e541b8cc8738a8ff80e62b0d1
BaoLong <phanhabaolong@gmail.com>
2024-10-02 10:42:55 +0700

    Update Ex3

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -165,22 +165,22 @@ void update7SEG(int index) {
 	switch (index) {
 
 	case 0:
-		// Display the first 7 SEG with led_buffer [0]
+
 		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 		display7SEG(led_buffer[0]);
 		break;
 	case 1:
-		// Display the second 7 SEG with led_buffer [1]
+
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
 		display7SEG(led_buffer[1]);
 		break;
 	case 2:
-		// Display the third 7 SEG with led_buffer [2]
+
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
 		display7SEG(led_buffer[2]);
 		break;
 	case 3:
-		// Display the forth 7 SEG with led_buffer [3]'
+
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
 		display7SEG(led_buffer[3]);
 		break;
