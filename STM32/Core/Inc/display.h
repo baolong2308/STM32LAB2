237ee448ca34e332c61057a0eaa2f4feeff11c94
BaoLong <phanhabaolong@gmail.com>
2024-10-02 11:17:41 +0700

    Update Ex9

---
+++ STM32/Core/Inc/display.h
@@ -0,0 +1,16 @@
+/*
+ * display.h
+ *
+ *  Created on: Oct 2, 2024
+ *      Author: admin
+ */
+
+#ifndef INC_DISPLAY_H_
+#define INC_DISPLAY_H_
+#include"main.h"
+void turnoffLEDs() ;
+void display7SEG(int num);
+void displayLEDMATRIX(int row, uint8_t col);
+void offrow_LEDMATRIX() ;
+
+#endif /* INC_DISPLAY_H_ */
