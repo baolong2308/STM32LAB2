dcb586ad56f5266e5b35e28d2ec6945cf4f2d342
BaoLong <phanhabaolong@gmail.com>
2024-10-03 13:22:10 +0700

    Update Ex5

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -478,7 +478,7 @@ static void MX_GPIO_Init(void) {
 /* USER CODE BEGIN 4 */
 
 int counter_dot = 100;
-int counter_led = 100;
+int counter_led = 1;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 
 	if (counter_led > 0) {
